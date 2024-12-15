1fd41991d6c1507800d822e0078736139b0ea4e1
Crenby <Crenby>
2024-12-15 13:55:22 +0700

    eslint corrections

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -73,7 +73,7 @@ const LoginForm = () => {
 
       <button type="submit" className="w-100 mb-3 btn btn-outline-primary">{t('enter')}</button>
     </form>
-  )
-}
+  );
+};
 
 export default LoginForm;
