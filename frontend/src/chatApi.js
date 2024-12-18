5e15afbfd891033e53ad2e145a58118fa8508685
Crenby <Crenby>
2024-12-18 19:01:06 +0700

    chatApi.js update

--- frontend/src/chatApi.js
+++ frontend/src/chatApi.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
-import { io } from 'socket.io-client';
 import store from './slices/index.js';
 
 const apiPath = '/api/v1';
-const socket = io();
 
 function getToken() {
   return store.getState().authorization.userToken;
@@ -26,8 +24,4 @@ export default {
   addMessage: (newMessage) => axios.post(`${apiPath}/messages`, newMessage, settingApi()),
   signup: (name, pass) => axios.post(`${apiPath}/signup`, { username: name, password: pass }),
   login: (name, pass) => axios.post(`${apiPath}/login`, { username: name, password: pass }),
-  socketNewMessage: (callback) => socket.on('newMessage', () => callback()),
-  socketNewChannel: (callback) => socket.on('newChannel', () => callback()),
-  socketRemoveChannel: (callback) => socket.on('removeChannel', () => callback()),
-  socketRenameChannel: (callback) => socket.on('renameChannel', () => callback()),
 };
