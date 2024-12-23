e62ff970fb20a8678b358c22b0bfa3108f018e3d
Crenby <Crenby>
2024-12-23 15:23:04 +0700

    modalDeleteChannel update

--- frontend/src/components/modals/modalDeleteChannel.jsx
+++ frontend/src/components/modals/modalDeleteChannel.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
+import { Button, Modal, Form } from 'react-bootstrap';
 import { setActiveChannel } from '../../slices/channelsSlice.js';
 import { setDeleteModalStatus } from '../../slices/modalsSlice.js';
 import chatApi from '../../chatApi.js';
@@ -24,54 +25,29 @@ const ModalDeleteChannel = () => {
   };
 
   return (
-    <>
-      <div className="fade modal-backdrop show" />
-      <div
-        role="dialog"
-        aria-modal="true"
-        className="fade modal show"
-        tabIndex="-1"
-        style={{ paddingRight: '17px', display: 'block' }}
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h4 className="modal-title">
-                {t('modals.removeChannel')}
-              </h4>
-              <button
-                onClick={closeModal}
-                type="button"
-                aria-label="Close"
-                data-bs-dismiss="modal"
-                className="btn btn-close"
-              />
-            </div>
-            <div className="modal-body">
-              <p className="lead">
-                {t('modals.questionInModal')}
-              </p>
-              <div className="d-flex justify-content-end">
-                <button
-                  onClick={closeModal}
-                  type="button"
-                  className="me-2 btn btn-secondary"
-                >
-                  {t('modals.cancelButton')}
-                </button>
-                <button
-                  onClick={removeChannel}
-                  type="button"
-                  className="btn btn-danger"
-                >
-                  {t('modals.removeButton')}
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <Modal show>
+      <Modal.Header>
+        <Modal.Title>
+          {t('modals.removeChannel')}
+        </Modal.Title>
+        <Button variant="close" onClick={closeModal} />
+      </Modal.Header>
+      <Modal.Body>
+        {t('modals.questionInModal')}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={closeModal}>
+          {t('modals.cancelButton')}
+        </Button>
+        <Button
+          variant="danger"
+          type="submit"
+          onClick={removeChannel}
+        >
+          {t('modals.removeButton')}
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
