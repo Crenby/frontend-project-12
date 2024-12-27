b8d9b69d13c22d4c280be83cfa5e154d9bf80531
Crenby <Crenby>
2024-12-27 14:10:52 +0700

    ligin.jsx update

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useLayoutEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Col, Container, Card, Row } from 'react-bootstrap';
 import LoginForm from './forms/LoginForm.jsx';
 import AvatarImage from '../assets/loginLogo.jfif';
 
@@ -20,34 +21,36 @@ const Login = () => {
   return (
     <>
       <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-        <div className="container">
-          <a className="navbar-brand" href="/"> Hexlet Chat</a>
-        </div>
+        <Container>
+          <a className="navbar-brand" href="/">
+            Hexlet Chat
+          </a>
+        </Container>
       </nav>
-      <div className="container-fluid h-100">
-        <div className="row justify-content-center align-content-center h-100">
-          <div className="col-12 col-md-8 col-xxl-6">
-            <div className="card shadow-sm">
-
-              <div className="card-body row p-5">
-                <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+      <Container className="h-100" fluid>
+        <Row className="justify-content-center align-content-center h-100">
+          <Col className="col-12 col-md-8 col-xxl-6">
+            <Card className="shadow-sm">
+              <Card.Body className="row p-5">
+                <Col className="col-12 col-md-6 d-flex align-items-center justify-content-center">
                   <img src={AvatarImage} className="rounded-circle" alt={t('enter')} />
-                </div>
+                </Col>
                 <LoginForm />
-              </div>
-
-              <div className="card-footer p-4">
+              </Card.Body>
+              <Card.Footer className="p-4">
                 <div className="text-center">
                   <span>
                     {t('notAccount')}
                   </span>
-                  <a href="/signup">{t('signUp')}</a>
+                  <a href="/signup">
+                    {t('signUp')}
+                  </a>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
