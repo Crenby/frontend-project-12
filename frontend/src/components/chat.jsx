19c3f7fe5dee5ce1fe385f61d97154df862d1c50
Crenby <Crenby>
2024-12-27 14:21:44 +0700

    chat.jsx update

--- frontend/src/components/chat.jsx
+++ frontend/src/components/chat.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
+import {
+  Container, Row, Button,
+} from 'react-bootstrap';
 import Messages from './messages.jsx';
 import Channels from './channels.jsx';
 import { authorization } from '../slices/authorizationSlice.js';
@@ -19,17 +22,17 @@ const Chat = () => {
   return (
     <>
       <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-        <div className="container">
+        <Container className="container">
           <a className="navbar-brand" href="/"> Hexlet Chat</a>
-          <button onClick={getOut} type="button" className="btn btn-primary">{t('exitButton')}</button>
-        </div>
+          <Button variant="primary" onClick={getOut} type="button">{t('exitButton')}</Button>
+        </Container>
       </nav>
-      <div className="container h-100 my-4 overflow-hidden rounded shadow">
-        <div className="row h-100 bg-white flex-md-row">
+      <Container className="h-100 my-4 overflow-hidden rounded shadow">
+        <Row className="h-100 bg-white flex-md-row">
           <Channels />
           <Messages />
-        </div>
-      </div>
+        </Row>
+      </Container>
     </>
   );
 };
