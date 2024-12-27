8fc6232770e82527cb9f6e9be9f10888cd647845
Crenby <Crenby>
2024-12-27 14:52:33 +0700

    PrivateRoute.jsx update

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -3,12 +3,12 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Chat from './chat';
 
-const isAuthenticated = () => {
-  const token = useSelector((state) => state.authorization.userToken);
-  return !!token;
-};
-
 const PrivateRoute = () => {
+  const isAuthenticated = () => {
+    const token = useSelector((state) => state.authorization.userToken);
+    return !!token;
+  };
+  
   return isAuthenticated() ? <Chat /> : <Navigate to="/login" />;
 };
 
