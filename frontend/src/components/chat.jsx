84fcf9e1361a32787fa799c5aa19e470d9cf3bc2
Crenby <Crenby>
2024-12-31 14:30:21 +0700

    header.jsx update

--- frontend/src/components/chat.jsx
+++ frontend/src/components/chat.jsx
@@ -3,18 +3,16 @@ import Messages from './messages.jsx';
 import Channels from './channels.jsx';
 import Header from './header.jsx';
 
-const Chat = () => {
-  return (
-    <>
-      <Header />
-      <Container className="h-100 my-4 overflow-hidden rounded shadow">
-        <Row className="h-100 bg-white flex-md-row">
-          <Channels />
-          <Messages />
-        </Row>
-      </Container>
-    </>
-  );
-};
+const Chat = () => (
+  <>
+    <Header />
+    <Container className="h-100 my-4 overflow-hidden rounded shadow">
+      <Row className="h-100 bg-white flex-md-row">
+        <Channels />
+        <Messages />
+      </Row>
+    </Container>
+  </>
+);
 
 export default Chat;
